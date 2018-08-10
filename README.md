4932bc25b043897974ee3b1907d69a92edc4cfaa
dandynaufaldi <dandy.naufaldi@gmail.com>
2018-08-10 18:27:37 +0700

    docstring on inception model

---
+++ README.md
@@ -0,0 +1,41 @@
+# Age and Gender Predictor
+**Update from old [repo](https://github.com/dandynaufaldi/Age-Gender-Predictor)**
+
+## Environment Setup
+### Docker with GPU support
+- Get [docker](https://docs.docker.com/install/) and [nvidia-docker](https://github.com/NVIDIA/nvidia-docker)
+- Pull my [docker image](https://hub.docker.com/r/dandynaufaldi/tf-keras-cuda9-cudnn7/) by `docker pull dandynaufaldi/tf-keras-cuda9-cudnn7`, the Dockerfile will come in future  
+### Manually
+Using python 3.5, core libraries are :
+- [dlib](https://github.com/davisking/dlib)
+- tensorflow
+- keras
+- opencv
+
+## Datasets
+Datasets are saved in data/ directory
+- [IMDB-Wiki](https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/)
+- [Adience](https://talhassner.github.io/home/projects/Adience/Adience-data.html)
+- [UTKFace](ttps://susanqq.github.io/UTKFace/)
+- [FGNET](http://yanweifu.github.io/FG_NET_data/index.html)
+
+## Preprocess
+
+
+## Train
+
+
+## Evaluation
+
+
+## Test
+
+
+## References and Acknowledgments
+This project is part of my internship program at [Nodeflux](nodeflux.io) as data scientist from July - August, 2018
+1. [Rothe R, Timofte R, Van Gool L. Dex: Deep expectation of apparent age from a single image[C]//Proceedings of the IEEE International Conference on Computer Vision Workshops. 2015: 10-15.](https://www.vision.ee.ethz.ch/en/publications/papers/proceedings/eth_biwi_01229.pdf)
+1. [Rothe R, Timofte R, Van Gool L. Deep expectation of real and apparent age from a single image without facial landmarks[J]. International Journal of Computer Vision, 2016: 1-14.](https://www.vision.ee.ethz.ch/en/publications/papers/articles/eth_biwi_01299.pdf)
+1. [[IJCAI18] SSR-Net: A Compact Soft Stagewise Regression Network for Age Estimation](https://github.com/shamangary/SSR-Net)
+1. [yu4u/age-gender-estimation Keras implementation of a CNN network for age and gender estimation](https://github.com/yu4u/age-gender-estimation)
+1. [deepinsight/insightface Face Recognition Project on MXNet](https://github.com/deepinsight/insightface)
+1. [abewley/sort Simple, online, and realtime tracking of multiple objects in a video sequence](https://github.com/abewley/sort)
--- model/inceptionv3.py
+++ model/inceptionv3.py
@@ -4,39 +4,82 @@ from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications.inception_v3 import InceptionV3
 from keras.utils import plot_model
 
+
 class AgenderNetInceptionV3(Model):
+    """Classification model based on InceptionV3 with 2 outputs, each for age and gender
+    """
+
     def __init__(self):
         self.input_size = 140
         base = InceptionV3(
-            input_shape=(140,140,3), 
-            include_top=False, 
-            weights='weight/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
+            input_shape=(140, 140, 3),
+            include_top=False,
+            weights=None)
         top_layer = GlobalAveragePooling2D(name='avg_pool')(base.output)
         gender_layer = Dense(2, activation='softmax', name='gender_prediction')(top_layer)
         age_layer = Dense(101, activation='softmax', name='age_prediction')(top_layer)
         super().__init__(inputs=base.input, outputs=[gender_layer, age_layer], name='AgenderNetInceptionV3')
-    
+
     def prep_phase1(self):
+        """Freeze layer from input until mixed10 (before last GlobalAveragePooling2D)
+        """
         for layer in self.layers[:311]:
             layer.trainable = False
-    
+        for layer in self.layers[311:]:
+            layer.trainable = True
+
     def prep_phase2(self):
+        """Freeze layer from input until mixed5
+        """
+        for layer in self.layers[:165]:
+            layer.trainable = False
         for layer in self.layers[165:]:
             layer.trainable = True
-    
+
     @staticmethod
     def decode_prediction(prediction):
+        """
+        Decode prediction to age and gender prediction.
+        Use softmax regression for age and argmax for gender.
+        Parameters
+        ----------
+        prediction : list of numpy array
+            Result from model prediction [gender, age]
+        Return
+        ----------
+        gender_predicted : numpy array
+            Decoded gender 1 male, 0 female
+        age_predicted : numpy array
+            Age from softmax regression
+        """
+
         gender_predicted = np.argmax(prediction[0], axis=1)
         age_predicted = prediction[1].dot(np.arange(0, 101).reshape(101, 1)).flatten()
         return gender_predicted, age_predicted
 
     @staticmethod
     def prep_image(data):
+        """Preproces image specific to model
+
+        Parameters
+        ----------
+        data : numpy ndarray
+            Array of N images to be preprocessed
+
+        Returns
+        -------
+        numpy ndarray
+            Array of preprocessed image
+        """
+
         data = data.astype('float16')
         data /= 127.5
         data -= 1.
         return data
 
+
 if __name__ == '__main__':
     model = AgenderNetInceptionV3()
     print(model.summary())
+    for (i, layer) in enumerate(model.layers):
+        print(i, layer.name)
